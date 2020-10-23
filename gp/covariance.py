ee8f6f81cf696b9dd92d42705ee86b49d6a2249b
uttambhat <uttambhat13@gmail.com>
2020-10-23 15:35:01 -0700

    Added gp/covariance.py

---
+++ gp/covariance.py
@@ -0,0 +1,20 @@
+import numpy as np
+import scipy as sp
+
+def covariance(X,additional_inputs):
+    """
+    
+
+    Parameters
+    ----------
+    X : (n x m) numpy array
+        The rows are the different datapoints and columns represent
+        the different features
+    additional_inputs : metric info
+        DESCRIPTION.
+
+    Returns
+    -------
+    covariance_matrix : (n x n) numpy array
+
+    """
