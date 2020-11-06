34e4a279e5955ce4f06693094dd0a12239dfcf93
uttambhat <uttambhat13@gmail.com>
2020-11-06 13:25:07 -0800

    Updated multivariate covariance function (no spatial/metric info yet)

--- gp/covariance.py
+++ gp/covariance.py
@@ -1,13 +1,16 @@
 import numpy as np
 from scipy.spatial.distance import cdist,pdist
 
-def covariance(X1,X2,phi,tau):
+def covariance(X1,X2,phi,tau=1.):
     """
     Parameters
     ----------
-    X : (n x m) numpy array
-        The rows are the different datapoints and columns represent
+    X1,X2 : (n x m) numpy array
+        The n rows are the different datapoints and the m columns represent
         the different features
+    phi : (m,) numpy array
+        Inverse length-scales 
+    tau : Amplitude of the squared exponential kernel
     additional_inputs : metric info
         DESCRIPTION.
 
