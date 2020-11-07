23d3ae7982758bfb82d9edc1e8fc7ed1c3c680d5
uttambhat <uttambhat13@gmail.com>
2020-11-06 17:07:28 -0800

    Incorporating ideas for the covariance function from 2020_11_06_Meeting

--- gp/covariance.py
+++ gp/covariance.py
@@ -1,24 +1,39 @@
 import numpy as np
-from scipy.spatial.distance import cdist,pdist
+from scipy.spatial.distance import pdist,squareform,cdist
 
-def covariance(X1,X2,phi,tau=1.):
+def covariance(X,inv_lengthscale,amplitude=1.,locations=None,spatial_decayrate=None,eval_gradient=False):
     """
+    Description
+    -----------
+    Calculates the covariance matrix.
+    
     Parameters
     ----------
-    X1,X2 : (n x m) numpy array
+    X : (n x m) numpy array or a tuple of (n1 x m) and (n2 x m) numpy arrays
         The n rows are the different datapoints and the m columns represent
         the different features
-    phi : (m,) numpy array
-        Inverse length-scales 
-    tau : Amplitude of the squared exponential kernel
-    additional_inputs : metric info
-        DESCRIPTION.
-
+    inv_lengthscale : scalar or (m,) shaped numpy array of inverse length-scales 
+    amplitude : Amplitude of the squared exponential kernel
+    locations : (n x k) numpy array of spatial locations or a tuple of (n1 x k) and (n2 x k) numpy arrays
+    spatial_decayrate : scalar or (k,) shaped numpy array of inverse length-scales for spatial locations
+    
     Returns
     -------
     covariance_matrix : (n x n) numpy array
-
+    
     """
-    return np.square(tau)*np.exp(-np.square(cdist(X1*phi,X2*phi)))
+    if type(X)!=tuple:
+        lnC = (pdist(X*inv_lengthscale))**2
+        if locations!=None:
+            lnC += (pdist(locations*spatial_decayrate))**2
+        
+        return squareform((amplitude**2)*np.exp(-0.5*lnC))
+        
+    else:
+        lnC=(cdist(X[0]*inv_lengthscale,X[1]*inv_lengthscale))**2
+        if locations!=None:
+            lnC += (cdist(locations[0]*spatial_decayrate,locations[1]*spatial_decayrate))**2
+        
+        return (amplitude**2)*np.exp(-0.5*lnC)
     
 
