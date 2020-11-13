3191fa663af1d0bc4b5bb245ac8fe7fed72264a4
uttambhat <uttambhat13@gmail.com>
2020-11-13 14:35:38 -0800

    Updated with return cov, gradient

--- gp/covariance.py
+++ gp/covariance.py
@@ -27,13 +27,17 @@ def covariance(X,inv_lengthscale,amplitude=1.,locations=None,spatial_decayrate=N
         if locations!=None:
             lnC += (pdist(locations*spatial_decayrate))**2
         
-        return squareform((amplitude**2)*np.exp(-0.5*lnC))
+        cov=squareform((amplitude**2)*np.exp(-0.5*lnC))
         
     else:
         lnC=(cdist(X[0]*inv_lengthscale,X[1]*inv_lengthscale))**2
         if locations!=None:
             lnC += (cdist(locations[0]*spatial_decayrate,locations[1]*spatial_decayrate))**2
         
-        return (amplitude**2)*np.exp(-0.5*lnC)
+        cov=(amplitude**2)*np.exp(-0.5*lnC)
     
-
+    if(eval_gradient):
+        
+        return cov,cov_gradient
+    else:
+        return cov
