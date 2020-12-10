5f2c510481fd61d15e5785b3d16962941d68bdeb
uttambhat <uttambhat13@gmail.com>
2020-12-10 12:32:31 -0800

    Added new file predict.py with docstrings. Code yet to be implemented.

--- gp/gpregression.py
+++ gp/gpregression.py
@@ -2,11 +2,13 @@ import numpy as np
 from .kernels import *
 
 class GPregressor:
-    def __init__(X,y,kernel=SquaredExponential,optimizer=rprop):
-        self.X = X #training data
-        self.y = y
+    def __init__(X_train,y_train,kernel=SquaredExponential,optimizer=rprop):
+        self.X = X_train
+        self.y = y_train
         self.kernel=kernel
         self.optimizer=optimizer
+
+    def predict(X
     
     def log_marginal_likelihood(self,parameters,eval_gradient=False):
         #... (self.kernel.covariance(self.X,parameters,eval_gradient)) ...
---
+++ gp/predict.py
@@ -0,0 +1,28 @@
+import numpy as np
+from covariance import *
+
+def predict(X,X_train,y_train,phi,tau=1.,return_std=False,return_cov=False):
+    """
+    Description
+    -----------
+    Calculates the mean predictions and (optionally) covariances at new points X
+    
+    Parameters
+    ----------
+    X_train : (n x m) numpy array of training data (independent variables)
+        The n rows are the different datapoints and the m columns represent
+        the different features
+    y_train : (n x 1) numpy array of training data (dependent variable)
+    phi : scalar or (m,) shaped numpy array of inverse length-scales 
+    tau : tau of the squared exponential kernel
+    return_std : Switch to return the standard deviations at the new points
+    return_cov : Switch to return the covariance matrix for the new points
+        
+    Returns
+    -------
+    y : (n x 1) numpy array of predictions
+    std_y : (n x 1) numpy array of standard deviations on predictions
+    cov_y : (n x n) numpy array of covariances on predictions
+    
+    """
+
